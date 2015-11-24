334d0846d80202f56c2236fc08beaf44c105efac
“Ian <ianyhkwok@hotmail.com>
2015-11-24 18:40:01 +0800

    callback to PRD link at facebook

--- config/passport.js
+++ config/passport.js
@@ -68,7 +68,7 @@ module.exports = function(passport) {
   passport.use('facebook', new FacebookStrategy({
     clientID        : process.env.FACEBOOK_API_KEY,
     clientSecret    : process.env.FACEBOOK_API_SECRET,
-    callbackURL     : 'http://localhost:3000/auth/facebook/callback',
+    callbackURL     : 'https://mybottle.herokuapp.com/auth/facebook/callback',
     enableProof     : true,
     profileFields   : ['name', 'emails']
   }, function(access_token, refresh_token, profile, done) {
